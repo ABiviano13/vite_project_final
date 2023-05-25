77422531beab79b03aa4134ba1d9aaf37e7694f4
ABiviano13 <abiviano0@outlook.com>
2023-05-25 09:10:23 +0200

    FEAT router.js

---
+++ src/router.js
@@ -0,0 +1,9 @@
+//import { createRouter, createWebHistory } from 'vue-router'
+
+//importiamo le rotte
+
+//const history = createWebHistory()
+
+//const router = createRouter
+
+//export { router }
