a8ffcc31081346c08857247202c45c4cc73003bd
ABiviano13 <abiviano0@outlook.com>
2023-06-05 13:01:55 +0200

    FEAT responsive show

--- src/pages/Teachers.show.vue
+++ src/pages/Teachers.show.vue
@@ -221,7 +221,30 @@
 <template>
     <Default>
             <div class="container">
-                <div class="row justify-content-between">
+                <div class="row justify-content-center align-items-center gap-4">
+                    <div class="col col-3 d-flex flex-column align-items-center">
+                        <img v-if="teacher.id <= 16" :src="teacher.picture" class="image_teacher" :alt="teacher.user.name">
+                        <img v-else :src="teacher.picture_path" :alt="teacher.user.name" class="image_teacher">
+
+                        <div class="info_teacher mt-2">
+                            <h3>
+                                Informazioni
+                            </h3>
+                            <p>
+                                Email: {{ teacher.user.email }}; <br>
+                                Telefono: {{ teacher.phone }}; <br>
+                                indirizzo: {{ teacher.user.address }}.
+                            </p>
+                            <div class="btn-group">
+                                <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                                    Curriculum Vitae
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-lg-end">
+                                    <img :src="teacher.cv" :alt="teacher.user.name">
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col col-8">
                         <div class="teacher_name">
                             <h1>
@@ -264,158 +287,121 @@
                                 </ul>
                             </div>
 
-                            <div class="reviews_teacher">
-                                <div class="accordion" id="accordionExample">
-                                    <div class="accordion-item">
-                                        <h2 class="accordion-header">
-                                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                                <h3 class="fw-bold">
-                                                    Recensioni
-                                                </h3>
-                                            </button>
-                                        </h2>
-                                        <ul id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                                            <li class="accordion-body" v-for="review in teacher.review">
-                                                <h5 class="fw-bold">
-                                                    {{ review.userReview }}
-                                                </h5>
-                                                {{ review.review }}                                    
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-
-                                <form @submit.prevent="sendReviewForm">
-                                    <p>
-                                        <input type="hidden" name="teacher_id" v-model="teacherId">
-                                    </p>
-                                    <p>
-                                        <input type="text" placeholder="Il tuo nome" name="user" v-model="userReview">
-                                        <small v-if="errors && errors.userReview" class="text-danger">
-                                            <span v-for="error in errors.userReview" :key="error">{{ error }}</span>
-                                        </small>
-                                    </p>
-                                    <p>
-                                        <textarea name="review" id="review" cols="60" rows="5" placeholder="Scrivi la tua recensione" class="input_style" v-model="review"></textarea>
-                                        <small v-if="errors && errors.review" class="text-danger">
-                                            <span v-for="error in errors.review" :key="error">{{ error }}</span>
-                                        </small>
-                                    </p>
-                                    <button v-if="loading === false" type="submit" class="input_style" :class=" !formReviewValidate() ? 'opacity-25' : ''">
-                                        Invia il messaggio
-                                    </button>
-                                    <div class="animate-pulse" v-else>sending...</div>
-                                </form>
-
-                                <form @submit.prevent="sendVoteForm">
-                                    <p>
-                                        <input type="hidden" name="teacher_id" v-model="teacherId">
-                                    </p>
-                                    <p>
-                                        <select name="vote_id" id="vote" v-model="selectedVote" :class="errors && errors.vote ? 'text-danger' : 'input_style'">
-                                            <option value="">Inserisci una valutazione da 1 a 5 al docente</option>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                        </select>
-                                        <button type="submit" class="input_style">
-                                            Invia la tua valutazione
-                                        </button>
-                                    </p>
-                                </form>
-
-                            </div>
-
                         </div>
 
-                    </div>
-                    <div class="col col-3">
-                        <img v-if="teacher.id <= 16" :src="teacher.picture" class="image_teacher" :alt="teacher.user.name">
-                        <img v-else :src="teacher.picture_path" :alt="teacher.user.name" class="image_teacher">
-
-                        <div class="info_teacher mt-2">
-                            <h3>
-                                Informazioni
-                            </h3>
-                            <p>
-                                Email: {{ teacher.user.email }}; <br>
-                                Telefono: {{ teacher.phone }}; <br>
-                                indirizzo: {{ teacher.user.address }}.
-                            </p>
-                            <div class="btn-group">
-                                <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-                                    Curriculum Vitae
-                                </button>
-                                <ul class="dropdown-menu dropdown-menu-lg-end">
-                                    <img :src="teacher.cv" :alt="teacher.user.name">
-                                </ul>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-12">
+                    <div class="reviews_teacher col-12">
+                        <div class="accordion" id="accordionExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        <h3 class="fw-bold">
+                                            Recensioni
+                                        </h3>
+                                    </button>
+                                </h2>
+                                <ul id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <li class="accordion-body" v-for="review in teacher.review">
+                                        <h5 class="fw-bold">
+                                            {{ review.userReview }}
+                                        </h5>
+                                        {{ review.review }}                                    
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <h5 class="mt-3">
+                            {{ teacher.user.name }} desidera una tua recensione.
+                        </h5>
+
+                        <form @submit.prevent="sendReviewForm" class="d-flex flex-column gap-1">
+                            <input type="hidden" name="teacher_id" v-model="teacherId">
+
+                            <input type="text" placeholder="Il tuo nome" name="user" v-model="userReview" class="input_style">
+                            <small v-if="errors && errors.userReview" class="text-danger">
+                                <span v-for="error in errors.userReview" :key="error">{{ error }}</span>
+                            </small>
+
+                            <textarea name="review" id="review" cols="60" rows="5" placeholder="Scrivi la tua recensione" class="input_style" v-model="review"></textarea>
+                            <small v-if="errors && errors.review" class="text-danger">
+                                <span v-for="error in errors.review" :key="error">{{ error }}</span>
+                            </small>
+
+                            <button v-if="loading === false" type="submit" class="input_style" :class=" !formReviewValidate() ? 'opacity-25' : ''">
+                                Invia
+                            </button>
+                            <div class="animate-pulse" v-else>sending...</div>
+                        </form>
+
+                        <h5 class="mt-3">
+                            {{ teacher.user.name }} desidera una tua valutazione.
+                        </h5>
+
+                        <form @submit.prevent="sendVoteForm" class="d-flex mt-3">
+                            <input type="hidden" name="teacher_id" v-model="teacherId">
+
+                            <select name="vote_id" id="vote" v-model="selectedVote" :class="errors && errors.vote ? 'text-danger' : 'input_style'">
+                                <option value="">Inserisci una valutazione da 1 a 5 al docente</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                            <button type="submit" class="input_style" :class=" selectedVote == 0 ? 'opacity-25' : ''">
+                                Invia
+                            </button>
+
+                        </form>
+
+                    </div>
+                    <div class="col-12 mt-5">
                         <h3>
                             Per prenotare, o annullare una prenotazione già effettuata, compila il seguente form:
                         </h3>
                         <div v-if="success" >
                             Messaggio inviato con successo!
                         </div>
-                        <form @submit.prevent="sendForm" class="d-flex flex-column gap-2">
+                        <form @submit.prevent="sendForm" class="d-flex flex-column gap-1">
 
-                            <div class="d-flex justify-content-around">
-                                <p>
-                                    <input type="hidden" name="teacher_id" v-model="teacherId">
-                                </p>
-                                <p>
-                                    <input type="text" name="ui_name" placeholder="Il tuo nome" :class="errors && errors.ui_name ? 'text-danger' : 'input_style'" v-model="ui_name">
-                                    <span>caratteri rimasti: {{ 100 - ui_name.length }}</span>
-                                    <small v-if="errors && errors.ui_name" class="text-danger">
-                                        <span v-for="error in errors.ui_name" :key="error">{{ error }}</span>
-                                    </small>
+                            <div class="d-flex flex-column">
+                                <input type="hidden" name="teacher_id" v-model="teacherId">
 
-                                </p>
+                                <input type="text" name="ui_name" placeholder="Il tuo nome" :class="errors && errors.ui_name ? 'text-danger' : 'input_style'" v-model="ui_name">
+                                <span>caratteri rimasti: {{ 100 - ui_name.length }}</span>
+                                <small v-if="errors && errors.ui_name" class="text-danger">
+                                    <span v-for="error in errors.ui_name" :key="error">{{ error }}</span>
+                                </small>
 
-                                <p>
+                                <input type="email" name="ui_email" placeholder="La tua email" :class="errors && errors.ui_email ? 'text-danger' : 'input_style'" v-model="ui_email">
+                                <span>caratteri rimasti: {{ 100 - ui_email.length }}</span>
+                                <small v-if="errors && errors.ui_email" class="text-danger">
+                                    <span v-for="error in errors.ui_email" :key="error">{{ error }}</span>
+                                </small>
 
-                                    <input type="email" name="ui_email" placeholder="La tua email" :class="errors && errors.ui_email ? 'text-danger' : 'input_style'" v-model="ui_email">
-                                    <span>caratteri rimasti: {{ 100 - ui_email.length }}</span>
-                                    <small v-if="errors && errors.ui_email" class="text-danger">
-                                        <span v-for="error in errors.ui_email" :key="error">{{ error }}</span>
-                                    </small>
+                                <input type="text" name="ui_phone" placeholder="Il tuo telefono" :class="errors && errors.ui_phone ? 'text-danger' : 'input_style'" v-model="ui_phone">
+                                <span>caratteri rimasti: {{ 100 - ui_phone.length }}</span>
 
-                                </p>
+                                <select name="title" id="title" v-model="title" :class="errors && errors.title ? 'text-danger' : 'input_style'">
+                                    <option value=""> La tua richiesta </option>
+                                    <option value="prenotazione">Richiesta di prenotazione</option>
+                                    <option value="annullamento">Richiesta di annullamento</option>
+                                    <option value="informazioni">Richiesta di informazioni</option>
+                                </select>
+                                <small v-if="errors && errors.title" class="text-danger">
+                                    <span v-for="error in errors.title" :key="error">{{ error }}</span>
+                                </small>
 
-                                <p>
-
-                                    <input type="text" name="ui_phone" placeholder="Il tuo telefono" :class="errors && errors.ui_phone ? 'text-danger' : 'input_style'" v-model="ui_phone">
-                                    <span>caratteri rimasti: {{ 100 - ui_phone.length }}</span>
-
-                                </p>
-
-                                <p>
-                                    <select name="title" id="title" v-model="title" :class="errors && errors.title ? 'text-danger' : 'input_style'">
-                                        <option value=""> La tua richiesta </option>
-                                        <option value="prenotazione">Richiesta di prenotazione</option>
-                                        <option value="annullamento">Richiesta di annullamento</option>
-                                        <option value="informazioni">Richiesta di informazioni</option>
-                                    </select>
-                                    <small v-if="errors && errors.title" class="text-danger">
-                                        <span v-for="error in errors.title" :key="error">{{ error }}</span>
-                                    </small>
-                                </p>
-
-                            </div>
-                            <p>
                                 <textarea name="text" id="text" cols="30" rows="10" v-model="text" :class="errors && errors.text ? 'text-danger' : 'input_style'">
                                     Scrivi il tuo messaggio
                                 </textarea>
                                 <small v-if="errors && errors.text" class="text-danger">
                                     <span v-for="error in errors.text" :key="error">{{ error }}</span>
                                 </small>
-                            </p>
+                            </div>
                             <button v-if="loading === false" type="submit" class="input_style" :class=" !formValidate() ? 'opacity-25' : ''">
                                 Invia il messaggio
                             </button>
